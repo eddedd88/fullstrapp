2616fbc6aa232071a81c20c1c62d29cf0496edc2
Eduardo Duran <eduardo.durandiaz@gmail.com>
2018-05-09 22:47:20 -0400

    Fix test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,5 +8,4 @@ it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
-  expect(false).toEqual(true)
 })
