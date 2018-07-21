cb2a505bcd9a9ad11894f7bd35991e1e349cabf3
Eduardo Duran <eduardo.durandiaz@gmail.com>
2018-07-21 17:12:35 -0400

    fix: page view analytics (#57)

--- src/utils/analytics/analytics.js
+++ src/utils/analytics/analytics.js
@@ -1,16 +1,14 @@
 // log it in dev and use gtag in prod
 let gaTrackingId = 'GA_TRACKING_ID'
 let tracker = console.log
+
 if (
   process.env.NODE_ENV === 'production' &&
   process.env.REACT_APP_GA_TRACKING_ID
 ) {
   gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID
-  global.gtag('js', new Date())
-  global.gtag('config', gaTrackingId, {
-    send_page_view: false
-  })
   tracker = global.gtag
+  tracker('js', new Date())
 }
 
 /**
@@ -34,6 +32,21 @@ export const pageViewed = ({
  * https://developers.google.com/analytics/devguides/collection/gtagjs/events
  */
 export const track = (
-  eventName: string,
-  parameters: { [string]: string }
-): void => tracker('event', eventName, parameters)
+  action: string,
+  {
+    category,
+    label,
+    value,
+    ...rest
+  }: {
+    category: string,
+    label: string,
+    value: number
+  }
+): void =>
+  tracker('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+    ...rest
+  })
