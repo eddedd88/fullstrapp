edcd8ac20f76b786ed17f92c15b2fd6dfb979697
Eduardo Duran <eduardo.durandiaz@gmail.com>
2019-11-05 09:00:00 -0500

    Move different templates into a single one * Replace Google Analytics with Firebase Analytics * Replace CircleCI with Github Actions

--- index.js
+++ index.js
@@ -8,46 +8,43 @@ const inquirer = require('inquirer')
 const deepmerge = require('deepmerge')
 const envfile = require('envfile')
 const execSync = require('child_process').execSync
-const packageJsonTemplate = require('./templates/packageJson.js')
-const tsConfigJsonTemplate = require('./templates/tsconfig.js')
+const packageJsonTemplate = require('./template/packageJson.js')
+const tsConfigJsonTemplate = require('./template/tsconfig.js')
 
 const appName = process.argv[2]
 const appDirectory = `${process.cwd()}/${appName}`
 
 const globalCommands = ['yarn']
 
-const packages = {
-  core: [
-    '@types/node',
-    '@types/react',
-    '@types/react-dom',
-    '@types/react-router-dom',
-    '@types/react-swipeable-views',
-    '@material-ui/core',
-    '@material-ui/icons',
-    'react-router',
-    'react-router-dom',
-    'react-swipeable-views',
-    'react-transition-group',
-    'typescript',
-    'typeface-roboto'
-  ],
-  firebase: ['firebase', 'firebaseui']
-}
+const packages = [
+  '@types/node',
+  '@types/react',
+  '@types/react-dom',
+  '@types/react-router-dom',
+  '@types/react-swipeable-views',
+  '@material-ui/core',
+  '@material-ui/icons',
+  'react-router',
+  'react-router-dom',
+  'react-swipeable-views',
+  'react-transition-group',
+  'typescript',
+  'typeface-roboto',
+  'firebase',
+  'firebaseui'
+]
 
-const devPackages = {
-  core: [
-    '@types/jest',
-    '@types/react-test-renderer',
-    'firebase-tools',
-    'husky',
-    'jest-localstorage-mock',
-    'lint-staged',
-    'prettier',
-    'react-test-renderer',
-    'source-map-explorer'
-  ]
-}
+const devPackages = [
+  '@types/jest',
+  '@types/react-test-renderer',
+  'firebase-tools',
+  'husky',
+  'jest-localstorage-mock',
+  'lint-staged',
+  'prettier',
+  'react-test-renderer',
+  'source-map-explorer'
+]
 
 const checkGlobalCommandsAreAvailable = () => {
   let aCommandIsMissing = false
@@ -81,40 +78,6 @@ const promptDestinationConfirmation = destination => {
   }
 }
 
-const promptCoreQuestions = defaultFirebaseProjectId =>
-  inquirer.prompt([
-    {
-      name: 'useFirebase',
-      type: 'confirm',
-      message: 'Would you like to include Firebase Database & Authentication?'
-    },
-    {
-      name: 'useGoogleAnalytics',
-      type: 'confirm',
-      message: 'Would you like to include Google Analytics to track app usage?'
-    },
-    {
-      name: 'firebaseProjectId',
-      type: 'input',
-      message: 'Enter your Firebase Project ID:',
-      default: defaultFirebaseProjectId,
-      validate: val => !!val || 'This is required in order to host your app.'
-    }
-  ])
-
-const promptFirebaseQuestions = defaultFirebaseApiKey =>
-  inquirer.prompt([
-    {
-      name: 'firebaseApiKey',
-      type: 'input',
-      message: 'Enter your Firebase Web API Key:',
-      default: defaultFirebaseApiKey,
-      validate: val =>
-        !!val ||
-        'This is required in order to connect to Firebase Database and Authentication locally'
-    }
-  ])
-
 const createReactApp = appName => {
   console.log('')
   execSync(`yarn create react-app ${appName} --typescript`, {
@@ -144,13 +107,12 @@ const installPackages = (packages, forDev) => {
   )
 }
 
-const installDependencies = setupType => {
-  installPackages(packages[setupType])
-  installPackages(devPackages[setupType], true)
+const installDependencies = () => {
+  installPackages(packages)
+  installPackages(devPackages, true)
 }
 
-const copyTemplates = setupType =>
-  fs.copy(`${__dirname}/templates/${setupType}`, `${appDirectory}`)
+const copyTemplate = () => fs.copy(`${__dirname}/template`, `${appDirectory}`)
 
 const enhanceJsonFile = (jsonFilePath, jsonTemplate) => {
   const jsonFile = fs.readJsonSync(jsonFilePath)
@@ -164,26 +126,6 @@ const enhanceJsonFile = (jsonFilePath, jsonTemplate) => {
   )
 }
 
-const updateFirebaseRc = firebaseProjectId =>
-  fs.readJson(`${appDirectory}/.firebaserc`, (err, firebaseRcFile) => {
-    const newFirebaseRcFile = deepmerge(firebaseRcFile, {
-      projects: {
-        default: firebaseProjectId
-      }
-    })
-
-    return fs.writeFile(
-      `${appDirectory}/.firebaserc`,
-      JSON.stringify(newFirebaseRcFile, null, 2) + os.EOL
-    )
-  })
-
-const createFirebaseEnvVars = ({ firebaseApiKey, firebaseProjectId }) =>
-  fs.writeFile(
-    `${appDirectory}/.env.local`,
-    `REACT_APP_FIREBASE_PROJECT_ID=${firebaseProjectId}\nREACT_APP_FIREBASE_API_KEY=${firebaseApiKey}`
-  )
-
 const createGitCommit = () => {
   execSync('git add -A', { stdio: 'ignore' })
   execSync('git commit -m "Initial commit from fullstrapp"', {
@@ -193,7 +135,7 @@ const createGitCommit = () => {
 }
 
 const run = async () => {
-  console.log(chalk.magenta('\nfullstrapping...\n'))
+  console.log(chalk.magenta('\nfullstrapping...'))
 
   if (!checkGlobalCommandsAreAvailable()) {
     console.log('Please install the missing global commands\n')
@@ -223,25 +165,9 @@ const run = async () => {
     fs.existsSync(`${appDirectory}/.env.local`) &&
     envfile.parseFileSync(`${appDirectory}/.env.local`)
 
-  const defaultFirebaseProjectId =
-    (firebaseRcJson && firebaseRcJson.projects.default) ||
-    (localEnvVars && localEnvVars.REACT_APP_FIREBASE_PROJECT_ID) ||
-    undefined
-
-  const {
-    firebaseProjectId,
-    useFirebase,
-    useGoogleAnalytics
-  } = await promptCoreQuestions(defaultFirebaseProjectId)
-
   let fbApiKey = localEnvVars
     ? localEnvVars.REACT_APP_FIREBASE_API_KEY
     : undefined
-
-  if (useFirebase) {
-    const { firebaseApiKey } = await promptFirebaseQuestions(fbApiKey)
-    fbApiKey = firebaseApiKey
-  }
 
   const updatingProject = fs.existsSync(appName)
   if (!updatingProject) {
@@ -257,24 +183,13 @@ const run = async () => {
     shell.rm('./src/App*')
   }
 
-  installDependencies('core')
-  await copyTemplates('core')
+  installDependencies()
+
+  await copyTemplate()
   await enhanceJsonFile(`${appDirectory}/package.json`, packageJsonTemplate)
   await enhanceJsonFile(`${appDirectory}/tsconfig.json`, tsConfigJsonTemplate)
-  await updateFirebaseRc(firebaseProjectId)
-
-  if (useFirebase) {
-    installDependencies('firebase')
-    await copyTemplates('firebase')
-    await createFirebaseEnvVars({
-      firebaseProjectId,
-      firebaseApiKey: fbApiKey
-    })
-  }
-
-  if (useGoogleAnalytics) {
-    await copyTemplates('analytics')
-  }
+  shell.rm('./packageJson.js')
+  shell.rm('./tsconfig.js')
 
   if (!updatingProject) {
     createGitCommit()
